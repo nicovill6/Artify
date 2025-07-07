30e2f7e0bcf87191e8e1c859e13a9740244b7728
DESKTOP-JDGUIRU\Usuario <46891285@fi.unju.edu.ar>
2025-07-07 03:02:57 -0300

    Set the base project

--- Artify/Artify/wsgi.py
+++ Artify/Artify/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mpDjangoIntroduccion project.
+WSGI config for Artify project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mpDjangoIntroduccion.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Artify.settings')
 
 application = get_wsgi_application()
